2c5a27be9870fd18f478e2e79fc7a37a9e17c2b8
peknight <JKpeknight@gmail.com>
2023-09-20 11:37:43 +0800

    update traverse

--- validation-core/shared/src/main/scala/com/peknight/validation/traverse/package.scala
+++ validation-core/shared/src/main/scala/com/peknight/validation/traverse/package.scala
@@ -1,9 +1,9 @@
 package com.peknight.validation
 
+import cats.Traverse
 import cats.data.Validated
 import cats.syntax.either.*
 import cats.syntax.traverse.*
-import cats.{Semigroup, Traverse}
 import com.peknight.error.Error
 import com.peknight.error.traverse.TraverseError.toTraverseError
 import com.peknight.error.traverse.{TraverseError, TraverseErrorShow}
@@ -14,8 +14,8 @@ package object traverse:
     : Either[TraverseError[F[A]], F[B]] =
       fa.traverse(a => f(a).toValidated).toEither.left.map(_.toTraverseError(label, fa, message))
 
-    def traverse[F[_] : Traverse, A, B](fa: F[A], label: => String)(using TraverseErrorShow[F[A]])
-                                       (f: A => Either[Error, B]): Either[TraverseError[F[A]], F[B]] =
+    def traverse[F[_], A, B](fa: F[A], label: => String)(f: A => Either[Error, B])
+                            (using Traverse[F], TraverseErrorShow[F[A]]): Either[TraverseError[F[A]], F[B]] =
       fa.traverse(a => f(a).toValidated).toEither.left.map(_.toTraverseError(label, fa))
   end either
 
@@ -24,8 +24,8 @@ package object traverse:
     : Validated[TraverseError[F[A]], F[B]] =
       fa.traverse(f).leftMap(_.toTraverseError(label, fa, message))
 
-    def traverse[F[_] : Traverse, A, B](fa: F[A], label: => String)(using TraverseErrorShow[F[A]])
-                                       (f: A => Validated[Error, B]): Validated[TraverseError[F[A]], F[B]] =
+    def traverse[F[_], A, B](fa: F[A], label: => String)(f: A => Validated[Error, B])
+                            (using Traverse[F], TraverseErrorShow[F[A]]): Validated[TraverseError[F[A]], F[B]] =
       fa.traverse(f).leftMap(_.toTraverseError(label, fa))
   end validated
 end traverse
