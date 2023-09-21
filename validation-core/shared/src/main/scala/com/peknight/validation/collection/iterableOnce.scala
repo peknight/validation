99db7ef243a66e8a88b4a2c892837677fb9525d3
peknight <JKpeknight@gmail.com>
2023-09-21 20:15:54 +0800

    fix bug

--- validation-core/shared/src/main/scala/com/peknight/validation/collection/iterableOnce.scala
+++ validation-core/shared/src/main/scala/com/peknight/validation/collection/iterableOnce.scala
@@ -9,24 +9,24 @@ object iterableOnce:
   object either:
     def nonEmpty[A, C <: IterableOnce[A]](c: C, label: => String, message: => String)
     : Either[CollectionEmptyError, C] =
-      if !c.iterator.hasNext then c.asRight[CollectionEmptyError]
+      if c.iterator.hasNext then c.asRight[CollectionEmptyError]
       else CollectionEmptyError(label, message).asLeft[C]
 
     def nonEmpty[A, C <: IterableOnce[A]](c: C, label: => String)(using CollectionEmptyErrorShow)
     : Either[CollectionEmptyError, C] =
-      if !c.iterator.hasNext then c.asRight[CollectionEmptyError]
+      if c.iterator.hasNext then c.asRight[CollectionEmptyError]
       else CollectionEmptyError(label).asLeft[C]
   end either
 
   object validated:
     def nonEmpty[A, C <: IterableOnce[A]](c: C, label: => String, message: => String)
     : Validated[CollectionEmptyError, C] =
-      if !c.iterator.hasNext then c.valid[CollectionEmptyError]
+      if c.iterator.hasNext then c.valid[CollectionEmptyError]
       else CollectionEmptyError(label, message).invalid[C]
 
     def nonEmpty[A, C <: IterableOnce[A]](c: C, label: => String)(using CollectionEmptyErrorShow)
     : Validated[CollectionEmptyError, C] =
-      if !c.iterator.hasNext then c.valid[CollectionEmptyError]
+      if c.iterator.hasNext then c.valid[CollectionEmptyError]
       else CollectionEmptyError(label).invalid[C]
   end validated
 
