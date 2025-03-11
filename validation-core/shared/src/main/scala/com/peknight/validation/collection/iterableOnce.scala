624a7465400800334dfc290bc9a95bb88c150db7
peknight <JKpeknight@gmail.com>
2025-03-11 17:28:24 +0800

    update

--- validation-core/shared/src/main/scala/com/peknight/validation/collection/iterableOnce.scala
+++ validation-core/shared/src/main/scala/com/peknight/validation/collection/iterableOnce.scala
@@ -14,9 +14,9 @@ object iterableOnce:
     def nonEmpty[A, C <: IterableOnce[A]](collection: C): Either[CollectionEmpty, C] =
       if collection.iterator.hasNext then collection.asRight[CollectionEmpty]
       else CollectionEmpty.asLeft[C]
-    def contains[A: Eq: Show, C <: IterableOnce[A]](value: A, collection: C): Either[CollectionNotContains[A, C], A] =
+    def contains[A: {Eq, Show}, C <: IterableOnce[A]](value: A, collection: C): Either[CollectionNotContains[A, C], A] =
       collection.iterator.find(_ === value).as(value).toRight(CollectionNotContains(value, collection))
-    def interact[A: Eq: Show, C1 <: IterableOnce[A], C2 <: IterableOnce[A]](c1: C1, c2: C2)
+    def interact[A: {Eq, Show}, C1 <: IterableOnce[A], C2 <: IterableOnce[A]](c1: C1, c2: C2)
     : Either[CollectionNoInteraction[A, C1, C2], C1] =
       if c1.iterator.exists(a => c2.iterator.exists(_ === a)) then c1.asRight
       else CollectionNoInteraction(c1, c2).asLeft
@@ -26,9 +26,9 @@ object iterableOnce:
     def nonEmpty[A, C <: IterableOnce[A]](collection: C): Validated[CollectionEmpty, C] =
       if collection.iterator.hasNext then collection.valid[CollectionEmpty]
       else CollectionEmpty.invalid[C]
-    def contains[A: Eq: Show, C <: IterableOnce[A]](value: A, collection: C): Validated[CollectionNotContains[A, C], A] =
+    def contains[A: {Eq, Show}, C <: IterableOnce[A]](value: A, collection: C): Validated[CollectionNotContains[A, C], A] =
       collection.iterator.find(_ === value).as(value).toValid(CollectionNotContains(value, collection))
-    def interact[A: Eq: Show, C1 <: IterableOnce[A], C2 <: IterableOnce[A]](c1: C1, c2: C2)
+    def interact[A: {Eq, Show}, C1 <: IterableOnce[A], C2 <: IterableOnce[A]](c1: C1, c2: C2)
     : Validated[CollectionNoInteraction[A, C1, C2], C1] =
       if c1.iterator.exists(a => c2.iterator.exists(_ === a)) then c1.valid
       else CollectionNoInteraction(c1, c2).invalid
--- validation-core/shared/src/main/scala/com/peknight/validation/collection/list.scala
+++ validation-core/shared/src/main/scala/com/peknight/validation/collection/list.scala
@@ -1,9 +1,10 @@
 package com.peknight.validation.collection
 
+import cats.Show
 import cats.data.{NonEmptyList, Validated}
 import cats.syntax.either.*
 import cats.syntax.validated.*
-import com.peknight.error.collection.CollectionEmpty
+import com.peknight.error.collection.{CollectionEmpty, CollectionError, MoreThenOneElement}
 
 object list:
   object either:
@@ -11,6 +12,11 @@ object list:
       list match
         case head :: tail => NonEmptyList(head, tail).asRight[CollectionEmpty]
         case Nil => CollectionEmpty.asLeft[NonEmptyList[A]]
+    def one[A: Show](list: List[A]): Either[CollectionError, A] =
+      list match
+        case head :: Nil => head.asRight[CollectionError]
+        case head :: tail => MoreThenOneElement(list).asLeft[A]
+        case _ => CollectionEmpty.asLeft[A]
   end either
 
   object validated:
@@ -18,6 +24,11 @@ object list:
       list match
         case head :: tail => NonEmptyList(head, tail).valid[CollectionEmpty]
         case Nil => CollectionEmpty.invalid[NonEmptyList[A]]
+    def one[A: Show](list: List[A]): Validated[CollectionError, A] =
+      list match
+        case head :: Nil => head.valid[CollectionError]
+        case head :: tail => MoreThenOneElement(list).invalid[A]
+        case _ => CollectionEmpty.invalid[A]
   end validated
 
 end list
